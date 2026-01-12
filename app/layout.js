a77ebe14baaf734bcfcc196aba35ad7ce8ae7dca
Elmokhtar1996 <abbou.elmokhtar96@gmail.com>
2026-01-12 23:42:24 +0100

    Fix horizontal scroll overflow + remove debug header

--- app/layout.js
+++ app/layout.js
@@ -28,7 +28,7 @@ export default function RootLayout({ children }) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       </head>
-      <body className="antialiased bg-white text-gray-900">
+      <body className="antialiased bg-white text-gray-900 overflow-x-hidden">
         <HeaderOptimized />
         <main className="pt-20">
           {children}
